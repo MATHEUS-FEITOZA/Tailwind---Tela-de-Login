13c722e1ba512a4d39de4b68ae48236714577ba0
MATHEUS-FEITOZA <matheusgimenez84@gmail.com>
2022-07-11 14:57:08 -0300

    commit com o projeto tela de login tailwind

---
+++ src/index.js
@@ -0,0 +1,19 @@
+let loginButton = document.getElementById("buttonLogin");
+
+let password = document.getElementById("password").value;
+let email = document.getElementById("email").value;
+
+// function Login(email, password) {
+    
+
+//     if (email == 'matheus_ct10@hotmail.com' && password == '123') {
+//         alert("Login com sucesso");
+        
+//     } else  {
+//         alert("Faha de Login");
+//     }
+        
+    
+// }
+
+// loginButton.addEventListener("click", Login);
