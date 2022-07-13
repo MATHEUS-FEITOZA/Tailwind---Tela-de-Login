ef370f74cfdd371c6364f678fa923e12a1843d98
MATHEUS-FEITOZA <matheus_ct10@hotmail.com>
2022-07-13 11:27:03 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 let loginButton = document.getElementById("buttonLogin");
 
-let password = document.getElementById("password").value;
-let email = document.getElementById("email").value;
 
-// function Login(email, password) {
+
+function Login(email, password) {
     
-
-//     if (email == 'matheus_ct10@hotmail.com' && password == '123') {
-//         alert("Login com sucesso");
-        
-//     } else  {
-//         alert("Faha de Login");
-//     }
-        
+    let password = document.getElementById("password").value;
+    let email = document.getElementById("email").value;
     
-// }
+    if (email == 'matheus_ct10@hotmail.com' && password == '123') {
+        alert("Login com sucesso");
 
-// loginButton.addEventListener("click", Login);
+    } else {
+        alert("Faha de Login");
+    }
+
+
+}
+loginButton.addEventListener("click", Login);
